ca4a43c1d4562b05b689e94385fbc16b10f75362
Manmeetlocowise <33830415+Manmeetlocowise@users.noreply.github.com>
2020-09-21 12:12:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function one(){
+    window.alert("THis monument is Taj Mahal ,it is located in agra.Mughal emperor Shah Jahan built Taj Mahal.");
+  }
+  function two(){
+     window.alert("THis monument is Great Wall of China,it is located in Huairou District, China.First Emperor Qin built Great Wall of China."); 
+  }
+  function four(){
+     window.alert("THis monument is Great Wall of China,it is located in Huairou District, China.First Emperor Qin built Great Wall of China."); 
+  }
+  
+  
+  
+  
+  
+  function three(){
+     window.alert("THis monument is Christ the Redeemer statue,it is located in Brazil."); 
+  }
+  function five(){
+     window.alert("THis monument is Great Wall of China,it is located in Huairou District, China.First Emperor Qin built Great Wall of China."); 
+  }
+  function six(){
+     window.alert("THis monument is Great Wall of China,it is located in Huairou District, China.First Emperor Qin built Great Wall of China."); 
+  }
+  function seven(){
+     window.alert("THis monument is Great Wall of China,it is located in Huairou District, China.First Emperor Qin built Great Wall of China."); 
+  }
